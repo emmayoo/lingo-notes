c96ac20e4da352bb75a53bfaacb570c4d5f7be5c
emmayoo <emmayoo9663@gmail.com>
2023-12-31 12:44:15 +1100

    ADD: styled-components theme with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 function App() {
-  return <div />;
+  return (
+    <Container>
+      <Title>Hello World!</Title>
+    </Container>
+  );
 }
 
 export default App;
